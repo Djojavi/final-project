c357336fc4aa574c514d06a9c784b5d47b434d9b
djojavi <djojavi19@gmail.com>
2026-04-10 10:23:48 -0500

    feat: user post and helper functions

--- final_project/src/routes/user.ts
+++ final_project/src/routes/user.ts
@@ -2,6 +2,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response, Application } from 'express';
 import prisma from '../../lib/prisma.ts';
+import { validateEmail, validateFieldsUser, validatePassword, hashPassword } from "../utils/user.utils.ts";
 
 export const userRouter = express.Router();
 
@@ -13,6 +14,43 @@ userRouter.get('/api/v1.0/users', async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
 
   res.json(users);
+});
+
+userRouter.post('/api/v1.0/users', async (req: Request, res: Response) => {
+  const email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const password = req.body.password;
+
+  if (!validateFieldsUser({ email, firstName, lastName, password })) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  if (!validateEmail(email)) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+
+  const passwordError = validatePassword(password);
+  if (passwordError !== "") {
+    return res.status(400).json({ message: passwordError });
+  }
+  const hashedPassword = await hashPassword(password);
+
+  const existingUser = await prisma.user.findUnique({ where: { email } });
+  if (existingUser) {
+    return res.status(409).json({ message: "Email already in use" });
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      firstName,
+      lastName,
+      password: hashedPassword,
+    }
+  });
+
+  res.status(201).json(newUser);
 });
 
 
--- final_project/src/server.ts
+++ final_project/src/server.ts
@@ -11,7 +11,7 @@ export const app: Application = express();
 const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
-
+app.use(express.json()); 
 
 app.use('/', userRouter);
 app.use('/', postRouter);

85f599957fb437eb537d8b372c2e6824dcf9c8f6
djojavi <djojavi19@gmail.com>
2026-04-14 14:13:18 -0500

    fix auth

--- final_project/src/middleware/auth.ts
+++ final_project/src/middleware/auth.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Application, NextFunction } from 'express';
+import express, { Request, Response , NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
